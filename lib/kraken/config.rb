38cd77a4c8de21192c17f9d83430151a032eead2
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-12-16 20:56:51 -0200

    Config triggers

--- lib/kraken/config.rb
+++ lib/kraken/config.rb
@@ -13,6 +13,7 @@ module Kraken
     include Singleton
 
     attr_reader :server_name, :server_token, :server_version
+    attr_reader :triggers
 
     def setup
       yield self
@@ -22,6 +23,13 @@ module Kraken
       @server_name = name
       @server_token = token
       @server_version = version
+    end
+
+    def add_trigger(klass: Kraken::Trigger)
+      raise 'Klass needs to inherit Kraken::Trigger' unless klass <= Kraken::Trigger
+
+      @triggers ||= {}
+      @triggers[klass.to_s.downcase] = klass
     end
   end
 end
