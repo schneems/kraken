9b4f486435c46b062cc298f3124fd4b98a600019
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-12-21 22:53:52 -0200

    Some tests

--- spec/kraken/handler_spec.rb
+++ spec/kraken/handler_spec.rb
@@ -1,7 +1,8 @@
 describe Kraken::Handler do
   class KrakenHandlerTest < Kraken::Trigger
     def run
-      @@params = params
+      @@params = params unless params.nil?
+      @@params = { a: 'b' } if params.nil?
     end
 
     def self.resp
@@ -45,6 +46,54 @@ describe Kraken::Handler do
     expect do
       @socket.puts 'krakenhandlertest'
       str = "h\n5\na\na\n2\nb\na\n3\nc\nh\n1\na\na\n5\nd\nv\n2\na\n2\nn\nk\nn"
+      @socket.puts str
+    end.to_not raise_error
+
+    sleep 1
+
+    expect(structure).to eq KrakenHandlerTest.resp
+  end
+
+  it 'can write nil' do
+    Kraken::Config.instance.add_trigger klass: KrakenHandlerTest
+
+    expect do
+      @socket.puts Faker::Dune.planet
+      @socket.puts Faker::Dune.planet
+      @ok = @socket.gets.chomp
+    end.to_not raise_error
+
+    expect(@ok).to eq 'ok'
+
+    structure = { a: 'b' }
+
+    expect do
+      @socket.puts 'krakenhandlertest'
+      str = "n"
+      @socket.puts str
+    end.to_not raise_error
+
+    sleep 1
+
+    expect(structure).to eq KrakenHandlerTest.resp
+  end
+
+  it 'can write []' do
+    Kraken::Config.instance.add_trigger klass: KrakenHandlerTest
+
+    expect do
+      @socket.puts Faker::Dune.planet
+      @socket.puts Faker::Dune.planet
+      @ok = @socket.gets.chomp
+    end.to_not raise_error
+
+    expect(@ok).to eq 'ok'
+
+    structure = []
+
+    expect do
+      @socket.puts 'krakenhandlertest'
+      str = "v\n0"
       @socket.puts str
     end.to_not raise_error
 
