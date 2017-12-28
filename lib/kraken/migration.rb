2767842efd056b76a40f9d74b82215a4382850be
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-12-28 13:44:55 -0200

    Using Active Record

--- lib/kraken/migration.rb
+++ lib/kraken/migration.rb
@@ -1,21 +1,12 @@
 module Kraken
-
   MIGRATION_DEFAULT = "
-  require 'kraken'
-  require 'require_all'
-
-  require_all app
-  require_all config
-
-  class Start < Kraken::Migration
-
-    def up; end
-
-    def down; end
-
-  end
-
-  Start.migrate(:up)
-  "
-
+require 'kraken'
+require 'require_all'\n
+require_all app
+require_all config\n
+class Start < Kraken::Migration
+  def up; end
+  def down; end
+end\n
+Start.migrate(:up)".freeze
 end
